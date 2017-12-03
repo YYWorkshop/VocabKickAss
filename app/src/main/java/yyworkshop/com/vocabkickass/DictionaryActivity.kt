0b7ff07b0071f9746caffdb414c9557abe160c67
dynamicy <fermat.little.theorem@gmail.com>
2017-12-03 16:07:16 +0800

    Add butterknife.

---
+++ app/src/main/java/yyworkshop/com/vocabkickass/DictionaryActivity.kt
@@ -0,0 +1,12 @@
+package yyworkshop.com.vocabkickass
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DictionaryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dictionary)
+    }
+}
